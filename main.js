6623d35af933214037aa9fdad237033516216a89
sufjanz <sufjanzogaj@gmail.com>
2025-12-18 18:42:44 +0100

    rfw

--- main.js
+++ main.js
@@ -13,7 +13,98 @@ closeBtn.onclick = function() {
      popup.style.display = "none";
 }
 
-var text="The most famous school is Digital School";
-var result=text.search("@gmail");
+function checkForm() {
 
-document.getElementById('form3Example4').innerHTML = result;
+  var firstName = document.getElementById("firstName").value;
+  var lastName = document.getElementById("lastName").value;
+  var email = document.getElementById("email").value;
+
+  // REGEX (BEGINNER LEVEL)
+  var nameRegex = /^[A-Za-z]+$/;
+  var emailRegex = /@.*\.com$/;
+
+  // FIRST NAME CHECK
+  if (nameRegex.test(firstName)) {
+    document.getElementById("firstNameError").style.display = "none";
+  } else {
+    document.getElementById("firstNameError").style.display = "block";
+  }
+
+  // LAST NAME CHECK
+  if (nameRegex.test(lastName)) {
+    document.getElementById("lastNameError").style.display = "none";
+  } else {
+    document.getElementById("lastNameError").style.display = "block";
+  }
+
+  // EMAIL CHECK
+  if (emailRegex.test(email)) {
+    document.getElementById("emailError").style.display = "none";
+  } else {
+    document.getElementById("emailError").style.display = "block";
+  }
+}
+
+// document.querySelector("form").addEventListener("submit", function (e) {
+//   e.preventDefault(); // ⛔ stops page refresh
+
+//   var firstName = document.getElementById("form3Example1").value;
+//   var lastName = document.getElementById("form3Example2").value;
+//   var email = document.getElementById("form3Example3").value;
+
+//   var nameRegex = /^[A-Za-z]+$/;
+//   var emailRegex = /@.*\.com$/;
+
+//   // FIRST NAME
+//   if (!nameRegex.test(firstName)) {
+//     document.getElementById("firstNameError").style.display = "block";
+//   } else {
+//     document.getElementById("firstNameError").style.display = "none";
+//   }
+
+//   // LAST NAME
+//   if (!nameRegex.test(lastName)) {
+//     document.getElementById("lastNameError").style.display = "block";
+//   } else {
+//     document.getElementById("lastNameError").style.display = "none";
+//   }
+
+//   // EMAIL
+//   if (!emailRegex.test(email)) {
+//     document.getElementById("emailError").style.display = "block";
+//   } else {
+//     document.getElementById("emailError").style.display = "none";
+//   }
+// });
+
+// document.querySelector("form").addEventListener("submit", function (e) {
+//   e.preventDefault();
+
+//   var firstName = document.getElementById("form3Example1").value;
+//   var lastName  = document.getElementById("form3Example2").value;
+//   var email     = document.getElementById("form3Example3").value;
+
+//   var numberRegex = /[0-9]/;   // ❗ NO g
+//   var emailRegex  = /@.*\.com$/;
+
+//   // FIRST NAME
+//   if (numberRegex.test(firstName)) {
+//     document.getElementById("firstNameError").style.display = "block";
+//   } else {
+//     document.getElementById("firstNameError").style.display = "none";
+//   }
+
+//   // LAST NAME
+//   if (numberRegex.test(lastName)) {
+//     document.getElementById("lastNameError").style.display = "block";
+//   } else {
+//     document.getElementById("lastNameError").style.display = "none";
+//   }
+
+//   // EMAIL
+//   if (!emailRegex.test(email)) {
+//     document.getElementById("emailError").style.display = "block";
+//   } else {
+//     document.getElementById("emailError").style.display = "none";
+//   }
+// });
